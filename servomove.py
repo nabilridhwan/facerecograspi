635d8b8c0dad25e24d080e3b61a3f255ca58ce9f
Nabil Ridhwanshah <imnabilridhwan@gmail.com>
2020-02-08 20:30:14 +0800

    Changes servomove

--- servomove.py
+++ servomove.py
@@ -10,6 +10,8 @@ p.start(7.5) # Initialization
 
 try:
     while True:
+        p.ChangeDutyCycle(7.5)
+        time.sleep(1)
         p.ChangeDutyCycle(12.5)
         time.sleep(1)
         p.ChangeDutyCycle(2.5)
