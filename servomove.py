eba09a2ef25173ce9793d5d6f211f66a6f61cc34
Nabil Ridhwanshah <imnabilridhwan@gmail.com>
2020-02-08 20:37:40 +0800

    Changes servomove

--- servomove.py
+++ servomove.py
@@ -6,7 +6,11 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 60) # GPIO 17 for PWM with 50Hz
-p.start(1.5) # Initialization
+p.start(7.5) # Initialization
+
+# We need a 1.5 ms pulse to centre the servo, or a Duty Cycle = 0.0015 * 50 = 0.075 (i.e 7.5%).
+# Similarly, 1 ms pulse (- 90 degrees) requires a Duty Cycle = 0.001 * 50 = 5%; and
+# 2 ms pulse (+ 90 degrees), Duty Cycle = 0.002 * 50 = 10%
 
 try:
     while True:
