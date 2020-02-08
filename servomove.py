a00280bd87a9e90e5378334631d80aa1efce18ba
Nabil Ridhwanshah <imnabilridhwan@gmail.com>
2020-02-08 20:49:04 +0800

    Changes servomove

--- servomove.py
+++ servomove.py
@@ -9,11 +9,11 @@ p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(7.5) # Initialization Reset Position
 # 10 - 90 degrees
 
-def unlock:
+def unlock():
     p.ChangeDutyCycle(7.5)
     time.sleep(1)
 
-def lock:
+def lock():
     p.ChangeDutyCycle(12.5)
     time.sleep(1)
 
