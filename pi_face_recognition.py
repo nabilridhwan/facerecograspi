a422bf4f58569f1dac881e3971d42155a4a6d14d
Nabil Ridhwanshah <imnabilridhwan@gmail.com>
2020-02-10 15:10:27 +0800

    Refactor and revert

--- pi_face_recognition.py
+++ pi_face_recognition.py
@@ -94,34 +94,25 @@ while True:
             name = max(counts, key=counts.get)
 
         if name != "Unknown":
-
-                    # loop over the recognized faces
-            for ((top, right, bottom, left), name) in zip(boxes, names):
-                # draw the predicted face name on the image
-                cv2.rectangle(frame, (left, top), (right, bottom),
-                            (0, 255, 0), 2)
-                y = top - 15 if top - 15 > 15 else top + 15
-                cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-                            0.75, (0, 255, 0), 2)
             unlock()
             buzzerSound(1)
         else:
-                    # loop over the recognized faces
-            for ((top, right, bottom, left), name) in zip(boxes, names):
-                # draw the predicted face name on the image
-                cv2.rectangle(frame, (left, top), (right, bottom),
-                            (255, 0, 0), 2)
-                y = top - 15 if top - 15 > 15 else top + 15
-                cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-                            0.75, (255, 0, 0), 2)
-
-            requests.get(HOST + "/recognize/unknown")
             lock()
             buzzerSound(2)
 
+        requests.get(HOST + "/recognize/" + name)
 
         # update the list of names
         names.append(name)
+
+        # loop over the recognized faces
+        for ((top, right, bottom, left), name) in zip(boxes, names):
+            # draw the predicted face name on the image
+            cv2.rectangle(frame, (left, top), (right, bottom),
+                        (0, 255, 0), 2)
+            y = top - 15 if top - 15 > 15 else top + 15
+            cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.75, (0, 255, 0), 2)
 
     
 
